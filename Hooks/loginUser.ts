071d59022c7021391f0489688bad75c7c70b3a61
NPrime01 <47795760+NPrime01@users.noreply.github.com>
2021-10-31 11:52:32 -0500

    removed comments

--- Hooks/loginUser.ts
+++ Hooks/loginUser.ts
@@ -3,7 +3,6 @@ const axios = require('axios');
 const url = 'http://192.168.1.43:3001/login';
 
 const loginUser = async (email: string, password: string) => {
-	// TODO: authenticate existing user from firestore
 	return axios.get(url, {
 		headers: {
 			email: email,
--- Hooks/registerUser.ts
+++ Hooks/registerUser.ts
@@ -3,7 +3,6 @@ import axios from "axios";
 const url = 'http://192.168.1.43:3001/login';
 
 const registerUser = async (email: string, password: string) => {
-	// TODO: register new user to firestore
 	return axios.post(url, {email: email, password: password})
 		.then((response: any) => JSON.stringify(response.data))
 		.catch((error: any) => alert(error.message));
