670b6c440700ac3e4e4f6c15711d2654ec21b94b
NPrime01 <47795760+NPrime01@users.noreply.github.com>
2021-11-08 16:48:50 -0600

    actually merged ryandev

--- Screens/Home.tsx
+++ Screens/Home.tsx
@@ -1,12 +1,7 @@
 import React, {useState, useEffect} from 'react'
 import {View, Text, TouchableOpacity, Button} from 'react-native'
 import getStorageData from '../Hooks/getStorageData'
-<<<<<<< HEAD
-import AsyncStorage from '@react-native-async-storage/async-storage';
-import getQuizes from '../Hooks/getQuizes'
-=======
 import AsyncStorage from '@react-native-async-storage/async-storage'
->>>>>>> ryandev
 
 type cCard = {
 	emotion: string,
@@ -80,7 +75,6 @@ export default function Home({navigation}) {
 			</TouchableOpacity>
 
 			<TouchableOpacity 
-<<<<<<< HEAD
 				style={{padding: 20, margin: 15, backgroundColor: 'lightblue', display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: 10}}	
 				onPress={async () => {
 					await AsyncStorage.removeItem('user');
@@ -92,19 +86,12 @@ export default function Home({navigation}) {
 
 			<TouchableOpacity 
 				style={{padding: 20, margin: 15, backgroundColor: 'red', display: 'flex', alignItems: 'center', justifyContent: 'center', borderRadius: 10}}	
-=======
-				style={{backgroundColor: 'red', padding: 20, borderRadius: 10, margin: 10, display: 'flex', justifyContent: 'center', alignItems: 'center'}}
->>>>>>> ryandev
 				onPress={async () => {
 					await AsyncStorage.clear();
 					navigation.goBack();
 				}}
 			>
-<<<<<<< HEAD
 				<Text style={{fontSize: 12, color: 'white'}}>Delete All Your Data</Text>
-=======
-				<Text style={{fontSize: 12, color: 'white'}}>DELETE USER DATA</Text>
->>>>>>> ryandev
 			</TouchableOpacity>
 		</View>
 	)
